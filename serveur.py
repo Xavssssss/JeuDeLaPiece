1083627dc46519e02c6ef4d68db6f8587f94a916
Xavssssss <xavymoullieres@yahoo.com>
2026-06-02 10:34:25 +0200

    Ajout casino client et serveur

--- serveur.py
+++ serveur.py
@@ -53,6 +53,10 @@ def index():
 def client():
     return render_template('client.html')
 
+@app.route('/clientCasino')
+def casino():
+    return render_template('casinoClient.html')
+
 # ---------------- SOCKET EVENTS ----------------
 
 @socketio.on('NouvJoueur')
@@ -121,6 +125,54 @@ def reset_room(data):
         rooms[room]["questions"] = charger_questions("question.txt")
         print("Room reset :", room)
         socketio.emit('majListe', [], to=room)
+
+
+################################################################## CASINO ###########################################################################
+import random
+import math
+from collections import Counter
+
+def roulette():
+    symboles = ["⭐", "🍋", "💎", "🍒","💀"]
+    poids = [40, 20, 15, 7, 3]
+
+    gains = {
+        "⭐": 2,
+        "🍋": 4,
+        "💎": 5,
+        "🍒": 7,
+        "💀":10
+    }
+
+    resultat = random.choices(symboles, weights=poids, k=3)
+
+    print(" | ".join(resultat))
+
+    # Compte le nombre d'apparitions de chaque symbole
+    compteur = Counter(resultat)
+
+    # Trouve le symbole le plus fréquent
+    symbole, nb = compteur.most_common(1)[0]
+    s1, s2, s3 = resultat
+    socketio.emit("symbole",{"s1":s1,"s2":s2,"s3":s3})
+    if nb == 3:
+        socketio.emit("Resultat",{"message":"🎉 JACKPOT ! Gain : " , "gain":gains[symbole]})
+        print(f"🎉 JACKPOT ! Gain : {gains[symbole]} pièces")
+
+    elif nb == 2:
+        gain = math.floor(gains[symbole] / 2)
+        socketio.emit("Resultat",{"message":f"✨ Deux symboles identiques ! Gain : " , "gain":gain})
+        print(f"✨ Deux symboles identiques ! Gain : {gain} pièces")
+    
+    else:
+        socketio.emit("Resultat",{"message":"❌ Perdu." , "gain":0})
+        print("❌ Perdu.")
+    return
+
+#--------SOCKET EVENT------------------
+@socketio.on('lancerRoulette')
+def lancerRoulette(data):
+    roulette()
 
 # ---------------- MAIN ----------------
 if __name__ == '__main__':
